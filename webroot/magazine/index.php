11e5b431e0680c35044932c51d2c6cec13d769ea
Felippe <felippemoraes@bitbucket.org>
2018-10-24 19:10:01 -0200

    Put the slash on the end of magazine ROOT URI without this slash, to cake do not understand it as a method

--- webroot/magazine/index.php
+++ webroot/magazine/index.php
@@ -13,5 +13,18 @@
  */
 define('WP_USE_THEMES', true);
 
+/** 
+	O cake identifica os enderecos sem / no fim como metodos, entao
+	geralmente o endereco dominio.com/magazine soh abre quando usado como dominio.com/magazine/.
+	Este hook verifica se a raiz esta sem / e redirecionad para magazine/ com barra no fim
+**/
+$base_uri = "magazine";
+$uri = explode("/", $_SERVER['REQUEST_URI']);
+if( $uri[1] != $base_uri ){
+	$address  = "http://" . $_SERVER['HTTP_HOST'] . "/magazine/";
+	header('Location: ' . $address, false);
+	die();
+}
+
 /** Loads the WordPress Environment and Template */
 require( dirname( __FILE__ ) . '/wp-blog-header.php' );
